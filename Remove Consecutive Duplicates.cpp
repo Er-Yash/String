ebb4b4e7e31ceba28828426607885ad9fe696b17
Yash Tripathi <68111412+Er-Yash@users.noreply.github.com>
2024-01-29 17:09:39 +0530

    Update Remove Consecutive Duplicates.cpp

--- Remove Consecutive Duplicates.cpp
+++ Remove Consecutive Duplicates.cpp
@@ -1,1 +1,17 @@
+string removeDuplicate(string &s)
+{
+    // Write your code here
+    int i=0;
+    while(i<s.length()-1){
+        if(s[i]==s[i+1]){
+            s.erase(i,1);
+        }
+        else{
+            i++;
+        }
+    }
+    return s;
+
+
+}
 
